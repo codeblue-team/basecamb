c51aca6118757c3bc7803a698005f47d642c31f9
Peter Marquardt <peter.marquardt@rwth-aachen.de>
2021-04-02 20:04:22 +0200

    Updated examples

--- R/Model_formula_tools.R
+++ R/Model_formula_tools.R
@@ -1,33 +1,33 @@
 #' Build formula for statistical models
-#' 
+#'
 #' Build formula used in statistical models from vectors of strings
-#' 
+#'
 #' @param outcome character denoting the column with the outcome
 #' @param predictors vector of characters denoting the columns with the predictors
 #' @param censor_event character denoting the column with the censoring event, for use in Survival-type models
-#' 
+#'
 #' @return formula for use in statistical models
-#' 
+#'
 #' @example build_model_formula('outcome', c('pred_1', 'pred_2'))
 #' @example build_model_formula('outcome', c('pred_1', 'pred_2'), censor_event='cens_event')
-#' 
+#'
 #' @importFrom assertive.types assert_is_character
-#' 
+#'
 #' @export
-#' 
+#'
 #' @author J. Peter Marquardt
 build_model_formula <- function(outcome, predictors, censor_event=NULL) {
-  
+
   assertive.types::assert_is_character(outcome)
   assertive.types::assert_is_character(predictors)
-  
+
   if(is.null(censor_event)) {  # standard formula
     frml <- as.formula(paste(outcome,
                              ' ~ ',
                              paste(predictors, collapse = ' + '),
                              sep = ''))
   }
-  
+
   else {  # Survival-type formula
     assertive.types::assert_is_character(censor_event)
     frml <- as.formula(paste('Surv(',
@@ -38,37 +38,36 @@ build_model_formula <- function(outcome, predictors, censor_event=NULL) {
                              paste(predictors, collapse = ' + '),
                              sep = ''))
   }
-  
+
   return(frml)
 }
 
 
 #' Deconstruct formula
-#' 
+#'
 #' Deconstruct a formula object into strings of its components
-#' 
+#'
 #' @param formula formula object for use in statistical models
-#' 
+#'
 #' @return a named list with fields
 #' * outcome (character)
 #' * predictors (vector of characters)
 #' * censor_event (character) (optional) censor event, only for formulas including a Surv() object
-#' 
+#'
 #' @examples
 #' deconstruct_formula(outcome ~ predictor1 + predictor2 + predictor3)
-#' require(Survival)
 #' deconstruct_formula(Surv(outcome, censor_event) ~ predictor)
-#' 
+#'
 #' @importFrom assertive.types assert_is_formula
-#' 
+#'
 #' @export
-#' 
-#' @author J. Peter Marquardt 
+#'
+#' @author J. Peter Marquardt
 deconstruct_formula <- function(formula){
-  
+
   # deparsing formula into string with no spaces and newlines
   form_string <- gsub(' ', '', gsub('\n', '', deparse1(formula, collapse = '')))
-  
+
   # extracting components
   if(substr(form_string, 1, 5) == 'Surv(') {  # Survival formula
     surv_string <- strsplit(form_string, ')')[[1]][1]  # Extracting the Surv() part of it
@@ -81,12 +80,12 @@ deconstruct_formula <- function(formula){
     censor_event <- NULL
   }
   predictors <- strsplit(strsplit(form_string, '~')[[1]][2], split='+', fixed = TRUE)[[1]] # same for all
-  
+
   # assembling output list
   component_list <- list('outcome' = outcome,
                          'predictors' = predictors)
   if(!is.null(censor_event)) {component_list$`censor_event` <- censor_event}
-  
+
   return(component_list)
 }
 
