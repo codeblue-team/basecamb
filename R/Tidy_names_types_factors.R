71ea31181a57ce9c46359ebbde89ba1587ba7c03
p-mq <peter.marquardt@rwth-aachen.de>
2021-04-03 00:23:07 +0200

    Added utility to tidy up names, types and factor levels of a data.frame, using functions - assign_types_names - assign_factorial_levels

---
+++ R/Tidy_names_types_factors.R
@@ -0,0 +1,134 @@
+#' Assign tidy types and names to a data.frame
+#'
+#' Verbosely assign tidy name and data type for each column of a data.frame and get rid of superfluous columns
+#' Uses a .csv file for assignments to encourage a data dictionary based workflow.
+#' Requires 'Date' type columns to already be read in as Date.
+#'
+#' @param data data.frame to be tidied. Dates must already be of type date
+#' @param meta_csv_path character path to csv with column names and datatypes. Has columns
+#' * old_column_name : character with the old column name
+#' * new_data_type : character denoting the tidy data type. Supported types are:
+#'   * character
+#'   * integer
+#'   * float
+#'   * factor
+#'   * date (can only confirm correct datatype assignment, not coerce other types into date)
+#' * new_column_name : tidy column name. Can be left blank to keep the old column name
+#' * Optional other columns (do not affect behaviour)
+#'
+#' @return clean data.frame
+#'
+#' @export
+#'
+#' @importFrom readr read_csv
+#'
+#' @author J. Peter Marqurdt
+assign_types_names <- function(data, meta_csv_path) {
+
+  assertive.types::assert_is_data.frame(data)
+
+  # reading in metadata
+  meta <- readr::read_csv(meta_csv_path,
+                          na = c(NA, "NA", ".", "#N/A")
+                          )
+
+  # filtering out unused columns
+  data_to_use <- meta[!is.na(meta$new_data_type),
+                      c("old_column_name", "new_data_type", "new_column_name")
+                      ]
+  filtered_data <- data[, data_to_use$old_column_name]
+
+  # assigning new names
+  data_to_use$new_column_name <- ifelse(!is.na(data_to_use$new_column_name),
+                                        data_to_use$new_column_name,
+                                        data_to_use$old_column_name
+                                        )
+  colnames(filtered_data) <- data_to_use$new_column_name
+
+  # assigning correct datatypes
+  for (i in seq_len(nrow(data_to_use))) {
+    if (data_to_use$new_data_type[i] == "character") {
+      filtered_data[data_to_use$new_column_name[i]] <-
+        as.character(filtered_data[[data_to_use$new_column_name[i]]])
+    }
+    else if (data_to_use$new_data_type[i] == "integer") {
+      filtered_data[data_to_use$new_column_name[i]] <-
+        as.integer(filtered_data[[data_to_use$new_column_name[i]]])
+    }
+    else if (data_to_use$new_data_type[i] == "float") {
+      filtered_data[data_to_use$new_column_name[i]] <-
+        as.double(filtered_data[[data_to_use$new_column_name[i]]])
+    }
+    else if (data_to_use$new_data_type[i] == "factor") {
+      filtered_data[data_to_use$new_column_name[i]] <-
+        as.factor(filtered_data[[data_to_use$new_column_name[i]]])
+    }
+    else if (data_to_use$new_data_type[i] == "date") {
+      filtered_data[data_to_use$new_column_name[i]] <-
+        as.Date(filtered_data[[data_to_use$new_column_name[i]]])
+    }
+    else {
+      warning(paste0('Type ',
+                     as.character(data_to_use$new_data_type[i]),
+                     'not recognized for column ',
+                     as.character(data_to_use$old_column_name[i]),
+                     '.  \nType remains ',
+                     as.character(typeof(filtered_data[data_to_use$new_column_name[i]]))
+                     )
+              )
+    }
+  }
+
+  #return
+  return(filtered_data)
+}
+
+
+#' Assign custom values for key levels in factorial columns
+#'
+#' Use a named vector of keys (current value) and values for factorial columns
+#' to assign meaningful levels and/or group levels
+#'
+#' @param data data.frame to modify
+#' @param factor_keys_values named list with:
+#' * Keys: Names of factor columns
+#' * values: Named vectors with
+#'    * keys: current value (string representation)
+#'    * values: new value to be assigned
+#'    * if a 'default' key is passed, all values not conforming to the new scheme will be converted to the 'default' value
+#'
+#' @return data frame with new levels
+#'
+#' @examples
+#' data <- data.frame(col1 = as.factor(rep(c('1', '2', '4'), 5)))
+#' keys_1 <- list('col1' = c('1' = 'One', '2' = 'Two', '4' = 'Four'))
+#' data_1 <- assign_factorial_levels(data, keys_1)
+#' keys_2 <- list('col1' = c('1' = 'One', 'default' = 'Not_One'))
+#' data_2 <- assign_factorial_levels(data, keys_2)
+#'
+#' @importFrom assertive.types assert_is_data.frame
+#' @importFrom assertive.types assert_is_list
+#'
+#' @export
+#'
+#' @author J. Peter Marqurdt
+assign_factorial_levels <- function(data, factor_keys_values) {
+
+  assertive.types::assert_is_data.frame(data)
+  assertive.types::assert_is_list(factor_keys_values)
+
+  for (col in names(factor_keys_values)) {
+    data[col] <- as.character.factor(data[[col]])
+    for (level in names(factor_keys_values[[col]])) {
+      data[col] <- ifelse(data[[col]] == level,
+                          factor_keys_values[[col]][[level]],
+                          data[[col]])
+      if (level == "default") { # assign default value to all fields that don't have a value yet
+        data[col] <- ifelse(!data[[col]] %in% unname(factor_keys_values[[col]]),
+                            factor_keys_values[[col]][[level]],
+                            data[[col]])
+      }
+    }
+  }
+  return(data)
+}
