caad0603461db8b6875f59cdebb58db10090896b
Peter Marquardt <peter.marquardt@rwth-aachen.de>
2021-04-11 17:20:32 +0200

    Added tests for assign_types_names

--- tests/testthat/test_assign_data_types_factors_dates.R
+++ tests/testthat/test_assign_data_types_factors_dates.R
@@ -41,3 +41,28 @@ test_that("assign_factorial_levels assigns factor levels correctly", {
   expect_error(assign_factorial_levels(data, list(col4 = c('1' = 'One'))),
                "attempting to coerce non-factor")
 })
+
+
+test_that("assign_types_names assigns correct data types", {
+  # Generating a dataset with missing values
+  data <- datasets::mtcars
+  data$date <- "4567-01-23"
+  data_dict <- data.frame(list("old_column_name" = c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb", "date"),
+                               "new_data_type" = c("float", "integer", "float", "float", "float", "float", "float", "factor", "factor", "integer", "integer", "date"),
+                               "new_column_name" = c("miles_per_gallon", "cylinders", "displacement", "horsepower",
+                                                     "rear_axle_ratio", "weight_lbs", "quarter_mile_time_secs",
+                                                     "engine_type", "transmission_type", "num_gears", "num_carburetors", "date"),
+                               "coding" = c(NA, NA, NA, NA, NA, NA, NA,
+                                            "'0' = 'V-shaped', '1' = 'straight', 'default' = NA",
+                                            "'0' = 'automatic', '1' = 'manual', 'default' = NA",
+                                            NA, NA, "%m/%d/%Y")
+                               )
+                          )
+  expect_equal(names(apply_data_dictionary(data=data, data_dictionary = data_dict)),
+               data_dict$new_column_name)
+  expect_true(is.integer(apply_data_dictionary(data=data, data_dictionary = data_dict)[['cylinders']]))
+  expect_true(is.double(apply_data_dictionary(data=data, data_dictionary = data_dict)[['horsepower']]))
+  expect_true(is.factor(apply_data_dictionary(data=data, data_dictionary = data_dict)[['transmission_type']]))
+  expect_equal(class(apply_data_dictionary(data=data, data_dictionary = data_dict)[['date']]), 'Date')
+  expect_equal(apply_data_dictionary(data=data, data_dictionary = data_dict)[['date']], rep(as.Date('4567-01-23'), 32))
+})
